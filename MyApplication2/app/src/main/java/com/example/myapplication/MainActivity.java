ab48cf58b7686d8f492334078649cd29b7562be7
Stepan <stepan@ponomarev.pp.ru>
2019-04-08 21:58:36 +0300

    Base application from Yandex getting started page. Called as MyApplication2

---
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.mapview.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mapview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        MapKitFactory.setApiKey("1d3edc87-53cd-460c-b6c6-926fabd6d13d");
+        MapKitFactory.initialize(this);
+
+        // Укажите имя activity вместо map.
+        setContentView(R.layout.activity_main);
+        mapview = (MapView)findViewById(R.id.mapview);
+        mapview.getMap().move(
+                new CameraPosition(new Point(55.751574, 37.573856), 11.0f, 0.0f, 0.0f),
+                new Animation(Animation.Type.SMOOTH, 0),
+                null);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapview.onStop();
+        MapKitFactory.getInstance().onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapview.onStart();
+        MapKitFactory.getInstance().onStart();
+    }
+}
